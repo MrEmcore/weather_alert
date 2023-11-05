6706fbdd890147aedce813f4445116e7ea10ec87
MrEmcore <ryan.hughes9@outlook.com>
2023-11-05 14:34:10 +0000

    Added Send Email

--- main.py
+++ main.py
@@ -1,35 +1,88 @@
-from second import mock_weather_response
+import os
+import requests
+import boto3
 
+
+# TODO: send_email
 
 class WeatherAlert:
 
-    def __init__(self, response_data):
-        self.response_data = response_data
+    def get_data(self, url, key, city, country):
+        r = requests.get(url=url, params={
+            "key": key,
+            "country": country,
+            "city": city
+        })
 
-    def get_title(self):
+        if r.status_code == 200:
+            return r.json()
+        else:
+            r.raise_for_status()
+
+    def check_for_alert(self, response_data):
+        """
+        If there is an alert it will return the title and severity. Otherwise it will not return anything
+        """
+        if len(response_data["alerts"]) > 0:
+            title = self.get_title(response_data=response_data)
+            severity = self.get_severity(response_data=response_data)
+            return title, severity
+        else:
+            return None, None
+
+    def get_title(self, response_data):
         """
 
         This function takes in the mock API response, and it returns the title for the region
         """
-        title = self.response_data["alerts"][0]["title"]
+        title = response_data["alerts"][0]["title"]
         return title
 
-    def get_severity(self):
+    def get_severity(self, response_data):
         """
 
         This function takes in the mock API response, and it returns the severity for the region
         """
-        return self.response_data["alerts"][0]["severity"]
+        return response_data["alerts"][0]["severity"]
+
+    def send_email(self, title, severity):
+        """
+        Sends email via AWS SNS if alert is present
+        """
+        if title:
+            client = boto3.client('sns', region_name="eu-west-2",
+                                  aws_access_key_id="",
+                                  aws_secret_access_key="")
+            response = client.publish(
+                TopicArn="arn:aws:sns:eu-west-2:416241265996:weather_alert",
+                Message=title,
+                Subject=f"Weather Alert {severity}"
+            )
+        else:
+            client = boto3.client('sns', region_name="eu-west-2",
+                                  aws_access_key_id="",
+                                  aws_secret_access_key="")
+            response = client.publish(
+                TopicArn="arn:aws:sns:eu-west-2:416241265996:weather_alert",
+                Message="Have a nice day",
+                Subject="Clear sky's"
+            )
+            print(response)
+
 
 
 if __name__ == "__main__":
 
-    # Initilize the class
-    weather_alert = WeatherAlert(response_data=mock_weather_response)
-    print(weather_alert.response_data)
+    weather_alert = WeatherAlert()
 
-    title = weather_alert.get_title()
-    severity = weather_alert.get_severity()
+    api_data = weather_alert.get_data(
+        url="https://api.weatherbit.io/v2.0/alerts",
+        key=os.getenv("WEATHER_API_KEY"),
+        city="Dundee",
+        country="GB"
+    )
+    print(api_data)
+    title, severity = weather_alert.check_for_alert(response_data=api_data)
+    print(title, severity)
 
-    print(title)
-    print(severity)
+    weather_alert.send_email(title=title, severity=severity)
